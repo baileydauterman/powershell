04fb1ba8060fc215a8a391a72c53166754343e47
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2019-12-20 15:58:00 -0500

    Create boilerplate-function.ps1

---
+++ boilerplate-function.ps1
@@ -0,0 +1,38 @@
+function boilerplate-powershellfunction {
+    <#
+    .SYNOPSIS 
+        Brief synopsis goes here
+    .DESCRIPTION 
+        Description goes here
+    .EXAMPLE 
+        Example 1
+    .EXAMPLE 
+        Example 2
+    .EXAMPLE
+        Example 3
+    .NOTES 
+        Created: Date Here
+        Last Updated: Date Here
+
+        Contributing Authors:
+            AuthorNameHere
+    .LINK
+       https://linktofunction.com/put_that_here
+    #>
+
+    [CmdletBinding()]
+    param(
+    )
+
+    begin {
+
+    }
+
+    process {
+
+    }
+
+    end {
+
+    }
+}
